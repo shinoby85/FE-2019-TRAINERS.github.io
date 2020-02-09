b3adbd777dba2ab07d6fad57f2fc7da4da7aa05c
shinoby <ponk@tut.by>
2020-02-09 19:01:18 +0300

    Compliate JS

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,20 @@ document.onscroll=function (event) {
     var scrollTop=window.pageYOffset || document.documentElement.scrollTop;
     //console.log(scrollTop);
     var navDesck=document.getElementById('hd-navigate');
-    var navIcon=document.getElementById('icon');
     var navItems=document.getElementsByClassName('header-desk__navigate__item');
+    var navIcon=document.getElementById('logo-icon');
     if (scrollTop>0){
-
         navDesck.style.backgroundColor="#fff";
         navIcon.style.fill='#000';
-
         for (var i=0;i<navItems.length;i++){
-            navItems[i].style.color='#000';
+            navItems[i].classList.add('nav-item-black');
         }
     }
     else{
         navDesck.style.backgroundColor="transparent";
         navIcon.style.fill='#fff';
         for (var i=0;i<navItems.length;i++){
-            navItems[i].style.color='#bfbfbf';
+            navItems[i].classList.remove('nav-item-black');
         }
-
     }
 }
