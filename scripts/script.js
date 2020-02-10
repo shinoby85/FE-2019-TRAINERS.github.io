d7f4ca421d1623f08d99d09621c862e796c87ac1
shinoby <ponk@tut.by>
2020-02-11 00:03:59 +0300

    Добавлена стилизация мобильному меню

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,53 @@
 document.onscroll=function (event) {
     var scrollTop=window.pageYOffset || document.documentElement.scrollTop;
     //console.log(scrollTop);
+    var mobiControl=document.getElementById('menu__control');
+        if ((scrollTop > 0) && !mobiControl.checked) {
+            changeStyle(true);
+        } else {
+            changeStyle(false);
+        }
+   // }
+};
+
+document.getElementById('btn-mobi').addEventListener('click',function () {
+    var scrollTop=window.pageYOffset || document.documentElement.scrollTop;
+    var mobiControl=document.getElementById('menu__control');
+    if ((scrollTop > 0) && mobiControl.checked) {
+        changeStyle(true);
+    } else {
+        changeStyle(false);
+    }
+});
+
+
+function changeStyle(flag){
     var navDesck=document.getElementById('hd-navigate');
+    var navMobi=document.getElementById('hm-navigate');
+    var mobiControl=document.getElementById('menu__control');
     var navItems=document.getElementsByClassName('header-desk__navigate__item');
-    var navIcon=document.getElementById('logo-icon');
-    if (scrollTop>0){
-        navDesck.style.backgroundColor="#fff";
-        navIcon.style.fill='#000';
-        for (var i=0;i<navItems.length;i++){
+    var navIcon=document.getElementsByClassName('header__logo_icon');
+    if (flag==true){
+        navDesck.style.backgroundColor = "#fff";
+        navMobi.style.backgroundColor = "#fff";
+        for (var i = 0; i < navIcon.length; i++) {
+            navIcon[i].style.fill = '#000';
+        }
+        for (var i = 0; i < navItems.length; i++) {
             navItems[i].classList.add('nav-item-black');
         }
     }
-    else{
-        navDesck.style.backgroundColor="transparent";
-        navIcon.style.fill='#fff';
-        for (var i=0;i<navItems.length;i++){
+    else {
+        navDesck.style.backgroundColor = "transparent";
+        navMobi.style.backgroundColor = "transparent";
+        for (var i = 0; i < navIcon.length; i++) {
+            navIcon[i].style.fill = '#fff';
+        }
+        for (var i = 0; i < navItems.length; i++) {
             navItems[i].classList.remove('nav-item-black');
         }
     }
 };
-
 
 
 
